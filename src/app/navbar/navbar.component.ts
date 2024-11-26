3c54a7fce1c077511b9bcbefe735c2237fe897da
CarloLaguda <carlo.laguda07@gmail.com>
2024-11-26 15:30:59 +0100

    Navbar operativa

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  Link1 = "Racing"
+  Link2 = "Cars"
+  Link3 = "Storia"
+  Link4 = "Musei"
+  Link5 = "Collezione"
+  Link6 = "Info"
+
+}
